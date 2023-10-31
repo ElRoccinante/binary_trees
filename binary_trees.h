ddc91a3367d5d2fcef418275b29205072e40189e
ElRoccinante <alaechouiye@gmail.com>
2023-10-31 10:33:30 -0700

    bettystyle_correction

--- binary_trees.h
+++ binary_trees.h
@@ -6,11 +6,12 @@
 
 /* Binary Tree Node Structure */
 typedef struct binary_tree_s binary_tree_t;
-struct binary_tree_s {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
